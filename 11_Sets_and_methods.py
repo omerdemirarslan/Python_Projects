7ee77f22317ccd2862118f88ef4430f55ae70b7f
omerdemirarslan <omerdemirarsln@gmail.com>
2020-04-19 19:29:15 +0300

    Sets in Python added

---
+++ 11_Sets_and_methods.py
@@ -0,0 +1,9 @@
+#Sets are unordered data types in Python.Run the print function in a row.
+#Each time you will see that the order in the terminal has changed.
+
+this_set = {"Python", "JavaScript", "Java"}
+print(this_set)
+
+#We can not reach the item of sets with index method because sets are unordered.
+
+print(this_set[0])
